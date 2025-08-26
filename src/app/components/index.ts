c499f92198f16e9532b9896f0ef2ceb1de7e6eb9
Eduardo-Vilela <90228644+Eduardo-Vilela@users.noreply.github.com>
2025-08-26 16:02:18 -0300

    ajuste error vercel

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,9 +1,2 @@
 // Layout Components
 export { Header } from './layout/Header';
-
-// Section Components
-export { Hero } from './sections/Hero';
-
-// UI Components (se pueden agregar aquí cuando se creen)
-// export { Button } from './ui/Button';
-// export { Card } from './ui/Card';
